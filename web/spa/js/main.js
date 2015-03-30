4e96618f497cca4a8857668cc2bffc55e830e413
Loïc Payol <loicpayol@gmail.com>
2015-03-30 15:19:17 +0200

    Configured jQuery

---
+++ web/spa/js/main.js
@@ -0,0 +1,23 @@
+/**
+ * Created by palra on 30/03/15.
+ */
+
+requirejs.config({
+  baseUrl: 'spa/js',
+  paths: {
+    jquery: './../../components/jquery/dist/jquery.min'
+  }
+});
+
+require([
+  'jquery'
+], function ($) {
+  $.noConflict();
+});
+
+
+require([
+  'jquery'
+], function ($) {
+  $('body').html('<h1>Hello world !</h1>')
+});
--- web/src/js/main.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by palra on 30/03/15.
- */
-
-require([], function() {
-  console.log('Hello world !');
-});
